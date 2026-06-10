334853d81cb31970ab8537928b89391aa1f2bff0
TunBet <hatemzaghwani@gmail.com>
2026-06-10 00:49:36 +0000

    deploy: new tunnel URL + no emojis

--- docs/config.js
+++ docs/config.js
@@ -1,3 +1,1 @@
-// TunBet API Configuration
-// Update this after deploying backend to Render or another permanent host
-// window.__TUNBET_SPORTSBOOK_API__ = "https://tunbet-sportsbook.onrender.com";
+window.__TUNBET_SPORTSBOOK_API__ = "https://memphis-fig-laptop-prince.trycloudflare.com";
